f1f9595bfbbcdb02ea62473c66e0f803371c9b3e
THALYSON RAFAEL <rafinha.head@gmail.com>
2025-07-17 23:41:39 -0300

    feat:create layout sidebar

---
+++ src/app/diary/layout.tsx
@@ -0,0 +1,16 @@
+import { AppSidebar } from "@/components/sidebar/app-sidebar";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <main className="container mx-auto min-h-screen px-4 sm:px-6 lg:px-8 relative">
+        <SidebarTrigger />
+        {children}
+      </main>
+    </SidebarProvider>
+  )
+}
