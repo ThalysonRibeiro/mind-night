5e332c3052a960231c593bd6bd11da4e57b235f1
THALYSON RAFAEL <rafinha.head@gmail.com>
2025-07-21 18:27:23 -0300

    feat: add scope role for user admin

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,22 @@
-import { withAuth } from 'next-auth/middleware'
+import { getToken } from 'next-auth/jwt';
+import { NextRequest, NextResponse } from 'next/server';
 
-export default withAuth(
-  function middleware(req) {
-    // Additional middleware logic can be added here
-    console.log('Middleware executed');
-    console.log(req);
+export async function middleware(req: NextRequest) {
+  const token = await getToken({ req });
 
-  },
-  {
-    callbacks: {
-      authorized: ({ token }) => !!token,
-    },
+  if (!token) {
+    return NextResponse.redirect(new URL('/login', req.url));
   }
-)
+
+  const isAdminRoute = req.nextUrl.pathname.startsWith('/admin');
+
+  if (isAdminRoute && token.role !== 'ADMIN') {
+    return NextResponse.redirect(new URL('/unauthorized', req.url)); // ou /dashboard etc.
+  }
+
+  return NextResponse.next();
+}
 
 export const config = {
-  matcher: ['/dashboard/:path*']
-}
+  matcher: ['/admin/:path*', '/diary/:path*'], // protege múltiplas rotas
+};
